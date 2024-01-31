525ef02d902777fad4e35e519ec326fd6cde0d29
Mijanur Rahman <mijanurcsebuet@gmail.com>
2024-01-31 13:05:53 +0600

    TO-DO list added

--- myCNN.py
+++ myCNN.py
@@ -1,36 +1,48 @@
+#                                   THESIS PROJECT - CONVOLUTIONAL NEURAL NETWORK
+#                                     Writer: Mijanur Rahman, Md. Faisal Zaman
+#               -------------------------------------------------------------------------------------------
+#               -------------------------------------------------------------------------------------------
 
-#CNN layer
+# sir k request kor j amra robibar joma dibo.......shoni r robi kaj korbo cnn niye
+
+#CNN layers
 #Input layer -> Convolutional layer -> Activation layer -> Pooling layer -> Flattening -> Fully Connected layers -> Output layer
 
 #Completed
 # - Input layer
 # - Convolutional layer
 # - Activation layer
+# - Pooling layer
+# - Flattening layer
+# - Fully Connected layer
+# - Output layer
 
+
+#TO-DO  List
+# - Iteration increasing -- See Documentation & Video Tutorial
+# - Linear Transform  -> Softmax
 
 #This is input layer.
 import numpy as np
 import keras
 from keras.datasets import cifar10
 
-# Load the CIFAR-10 dataset
+# Loading the CIFAR-10 dataset
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
 x_train_shape = 1000
 x_train = x_train[0:x_train_shape]
 y_train = y_train[0:x_train_shape]
 
-# Normalize pixel values between 0 and 1
+# Normalizing pixel values between 0 and 1
 x_train = x_train.astype('float32') / 255.0
 x_test = x_test.astype('float32') / 255.0
 
-# Reshape the labels to be one-dimensional arrays
+# Reshaping the labels to be one-dimensional arrays
 y_train = np.squeeze(y_train)
 y_test = np.squeeze(y_test)
 
-# Define the input shape
 
-# Print an example output of a single image
 image_height, image_width, num_channels = x_train[0].shape
 print(x_train[0])
 print(image_height, image_width, num_channels)
@@ -59,9 +71,8 @@ def conv2d(image, kernel):
 
 filter_size = 3
 num_channels = 3
-# num_filters = 32
 
-# Create a random filter
+# Creating a random filter
 kernel = np.random.randn(filter_size, filter_size, num_channels)
 print(kernel)
 # filter_biases = np.random.randn(num_filters)
@@ -87,22 +98,21 @@ x_activation = x_convolutional
 #Pooling layer
 
 def pooling_layer(input_data, pool_size=2, pool_type='max'):
-    # Get dimensions
     input_height = len(input_data)
     input_width = len(input_data[0])
     pool_height, pool_width = pool_size, pool_size
     
 
-    # Calculate output dimensions
+    # Calculating output dimensions
     foutput_height = input_height // pool_height
     foutput_width = input_width // pool_width
     output_height=int(foutput_height)
     output_width=int(foutput_width)
 
-    # Initialize output feature map
+    # Initializing output feature map
     output = np.zeros((output_height, output_width))
 
-    # Perform pooling
+    # Performing pooling
     for i in range(output_height):
         for j in range(output_width):
                 if pool_type == 'max':
